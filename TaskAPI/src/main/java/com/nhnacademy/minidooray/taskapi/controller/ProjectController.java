095a76524471610578e91746ba0c6708885a91d6
codeTheStudent <hjun303@gnu.ac.kr>
2024-02-22 16:44:59 +0900

    feat: entity classes association implemented

---
+++ TaskAPI/src/main/java/com/nhnacademy/minidooray/taskapi/controller/ProjectController.java
@@ -0,0 +1,2 @@
+package com.nhnacademy.minidooray.taskapi.controller;public class ProjectController {
+}
---
+++ TaskAPI/src/main/java/com/nhnacademy/minidooray/taskapi/domain/ProjectState.java
@@ -0,0 +1,2 @@
+package com.nhnacademy.minidooray.taskapi.domain;public enum ProjectState {
+}
