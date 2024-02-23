364838fa6d085febfd285de9737d48a0e422a627
insub2004 <insub2004@naver.com>
2024-02-23 17:38:57 +0900

    Controller 추가2

--- Gateway/src/main/java/com/nhnacademy/minidooray/gateway/controller/account/LoginController.java
+++ Gateway/src/main/java/com/nhnacademy/minidooray/gateway/controller/account/LoginController.java
@@ -1,18 +1,19 @@
 package com.nhnacademy.minidooray.gateway.controller.account;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.nhnacademy.minidooray.gateway.domain.AccountDto;
 import com.nhnacademy.minidooray.gateway.domain.LoginRequest;
+import com.nhnacademy.minidooray.gateway.domain.SignupRequest;
 import com.nhnacademy.minidooray.gateway.service.account.AccountService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
-import javax.websocket.Session;
 import java.util.Objects;
 
 @Slf4j
@@ -27,17 +28,29 @@ public class LoginController {
     }
 
     @GetMapping("/login")
-    public String getLogin(){
+    public String getLogin() {
         return "login";
     }
 
+
+    @GetMapping("/signup")
+    public String getLoginForm() {
+        return "signup";
+    }
+
     @PostMapping("/login")
-    public String tryToLogin(@ModelAttribute LoginRequest loginRequest, BindingResult bindingResult, HttpServletRequest httpServletRequest, Model model) throws Exception {
+    public String tryToLogin(@ModelAttribute LoginRequest loginRequest, BindingResult bindingResult, HttpServletRequest httpServletRequest, Model model, RedirectAttributes redirectAttributes) throws Exception {
+
+        log.info("loginRequest id : {} / pw : {}", loginRequest.getId(), loginRequest.getPassword());
 
         AccountDto accountDto = accountService.getAccount(loginRequest);
 
+        log.info("accountDto from Account-api : {}",accountDto);
+
         if (Objects.isNull(accountDto)) {
-            return "redirect:/login";
+            log.info("redirectAttributes Test");
+            redirectAttributes.addFlashAttribute("message", "로그인 실패");
+            return "redirect:/account/login";
         }
 
         HttpSession session = httpServletRequest.getSession(true);
@@ -45,16 +58,22 @@ public class LoginController {
         session.setAttribute("account",accountDto);
         session.setMaxInactiveInterval(2*60*60);
 
-        model.addAttribute("account", accountDto);
-        return "dooray";
+        //redirectAttributes.addFlashAttribute("account", accountDto);
+        return "redirect:/task/dooray";
     }
 
     @GetMapping("/logout")
-    public String tryToLogout(HttpSession session) {
+    public String tryToLogout(HttpServletRequest request, @CookieValue("JSESSIONID") Cookie cookie) {
 
+        //todo 로그인 or 로그아웃 시간 추가해야하나?
+
+        HttpSession session = request.getSession(false);
+
+        cookie.setMaxAge(0);
         session.invalidate();
+        session.setMaxInactiveInterval(0);
 
-        return "redirect:/login";
+        return "redirect:/";
     }
 
 }
