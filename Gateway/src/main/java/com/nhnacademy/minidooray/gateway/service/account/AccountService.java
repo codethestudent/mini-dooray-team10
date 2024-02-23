f0ef4bc6b8d5b3b0b201439c72191720049a3ff3
insub2004 <insub2004@naver.com>
2024-02-23 17:35:53 +0900

    AccountService, AccountServiceImpl 추가

--- Gateway/src/main/java/com/nhnacademy/minidooray/gateway/service/account/AccountService.java
+++ Gateway/src/main/java/com/nhnacademy/minidooray/gateway/service/account/AccountService.java
@@ -2,9 +2,12 @@ package com.nhnacademy.minidooray.gateway.service.account;
 
 import com.nhnacademy.minidooray.gateway.domain.AccountDto;
 import com.nhnacademy.minidooray.gateway.domain.LoginRequest;
+import com.nhnacademy.minidooray.gateway.domain.SignupRequest;
 
 public interface AccountService {
 
     AccountDto getAccount(LoginRequest loginRequest);
+
+    AccountDto createAccount(SignupRequest signupRequest);
 
 }
--- Gateway/src/main/java/com/nhnacademy/minidooray/gateway/service/account/AccountServiceImpl.java
+++ Gateway/src/main/java/com/nhnacademy/minidooray/gateway/service/account/AccountServiceImpl.java
@@ -3,6 +3,7 @@ package com.nhnacademy.minidooray.gateway.service.account;
 import com.nhnacademy.minidooray.gateway.adaptor.account.AccountAdaptor;
 import com.nhnacademy.minidooray.gateway.domain.AccountDto;
 import com.nhnacademy.minidooray.gateway.domain.LoginRequest;
+import com.nhnacademy.minidooray.gateway.domain.SignupRequest;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -18,4 +19,11 @@ public class AccountServiceImpl implements AccountService {
     public AccountDto getAccount(LoginRequest loginRequest) {
         return accountAdaptor.loginExistUser(loginRequest);
     }
+
+    @Override
+    public AccountDto createAccount(SignupRequest signupRequest) {
+        return accountAdaptor.createUser(signupRequest);
+    }
+
+
 }
